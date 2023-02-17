a50411e9aa11a44f4ad828cef9966a8ccbde5687
Yamato Sanekata <yamatomrp305@gmail.com>
2023-02-17 15:37:03 +0900

    delete jquery code

--- 11MenuK.php
+++ 11MenuK.php
@@ -101,17 +101,3 @@
 </body>
 
 </html>
-
-<script>
-    $(function () {
-        var menu = $('#nav'),
-            offset = menu.offset();
-        $(window).scroll(function () {
-            if ($(window).scrollTop() > offset.top) {
-                menu.addClass('fixed');
-            } else {
-                menu.removeClass('fixed');
-            }
-        });
-    });
-</script>
