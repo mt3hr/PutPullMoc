8bf921a88c634e7e95ba52f16beaa2762a98ddd1
Yamato Sanekata <yamatomrp305@gmail.com>
2023-02-17 14:40:12 +0900

    clean

--- putpullmoc/test copy.sql
+++
@@ -1,8 +0,0 @@
--- DROP TABLE passwordReset;
--- CREATE TABLE passwordReset (
---  Email CHAR(36) NOT NULL,
---  token CHAR(36) NOT NULL,
---  ResetSentAt CHAR(50) NOT NULL
-
--- );
-SELECT * FROM userTable;
