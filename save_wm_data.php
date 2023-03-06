1f127da66370a47fffaf45ad24f1cdf81eaa32fa
Yamato Sanekata <ymt3shr@gmail.com>
2023-03-06 13:35:46 +0900

    卒制用モックアップ保存日時記録機能修正

--- save_wm_data.php
+++ save_wm_data.php
@@ -13,23 +13,22 @@ $json = file_get_contents('php://input');
 $jsondata = json_decode($json, true);
 $wm_id = $jsondata["wm_id"];
 $wm_name = $jsondata["wm_name"];
-$owner_user_id = $jsondata["owner_user_id"] != "" ? $jsondata["owner_user_id"] : $user_id;
+$owner_user_id = $jsondata["owner_user_id"] != "" ? $jsondata["owner_user_id"] : $_SESSION['userID'];
 $register_date_time = $jsondata["register_date_time"];
 $html_with_id = $jsondata["html_with_id"];
 
-$user_id = $owner_user_id != "" ? $owner_user_id:  $userID = $_SESSION['userID'];
-$next_version_id = "1"; // バージョンはサーバでシーケンシャルな値を設定していくという話に落ち着いていたはず。
+$user_id = $owner_user_id;
+$next_version_id = 1; // バージョンはサーバでシーケンシャルな値を設定していくという話に落ち着いていたはず。
 $sql = "SELECT TOP 1 VersionID FROM mockup WHERE WMID = ? AND UserID = ? ORDER BY VersionID DESC;";
 $stmt = $pdo->prepare($sql);
 $stmt->execute(array($wm_id, $owner_user_id));
+var_dump($wm_id);
+var_dump($owner_user_id);
 $count = $stmt->rowCount();
-if ($count != 0) {
-    while ($row = $stmt->fetch(PDO::FETCH_BOTH)) {
-        $next_version_id = ((int) $row["VersionID"]) + 1;
-    }
-} else {
-    $next_version_id = 1;
-}
+$result = $stmt->fetchAll(PDO::FETCH_BOTH);
+var_dump($result);
+$next_version_id = ($result[0]["VersionID"]) + 1;
+var_dump($next_version_id);
 
 // 保存処理
 $sql = 'INSERT INTO mockup(WMID, VersionID, WMName, RegisterDatetime, HTMLWithID, UserID) VALUES(?, ?, ?, ? ,? ,?);';
